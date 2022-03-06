4cee8fa1bd24a4edf6aacded3cd65bc499aa9f92
Anju Chamantha <chamantha97anju@gmail.com>
2022-03-06 20:18:33 +0530

    Change endpoint params

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 ## Run the live API server
 In terminal (main.py level): \
 uvicorn main:app --reload
+
+## Heroku Deploy
+heroku login
+heroku git:remote -a msense-server
+heroku git:remote -a msense-server
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def root():
             "API Specification": "/redoc#/"}
 
 
-class PredictionData(BaseModel):
+class SensedData(BaseModel):
     acc_x: float
     acc_y: float
     acc_z: float
@@ -48,31 +48,31 @@ class PredictionData(BaseModel):
     screen_off_count: float
 
 
-@app.post("/predict/")
-async def test(prediction_data: PredictionData):
+@app.post("/predict")
+async def predict(sensed_data: SensedData):
     msg = "Prediction data received"
-    data = [prediction_data.acc_x, prediction_data.acc_y, prediction_data.acc_z, prediction_data.acc_x_bef,
-            prediction_data.acc_y_bef, prediction_data.acc_z_bef, prediction_data.acc_x_aft, prediction_data.acc_y_aft,
-            prediction_data.acc_z_aft,
-            prediction_data.acc_xabs, prediction_data.acc_yabs, prediction_data.acc_zabs, prediction_data.acc_xabs_bef,
-            prediction_data.acc_yabs_bef, prediction_data.acc_zabs_bef,
-            prediction_data.acc_xabs_aft,
-            prediction_data.acc_yabs_aft, prediction_data.acc_zabs_aft, prediction_data.battery_level,
-            prediction_data.charging_true_count,
-            prediction_data.charging_ac, prediction_data.charging_usb,
-            prediction_data.charging_unknown, prediction_data.minutes_elapsed, prediction_data.hours_elapsed,
-            prediction_data.weekend, prediction_data.radius_of_gyration,
-            prediction_data.screen_on_count, prediction_data.screen_off_count]
+    data = [sensed_data.acc_x, sensed_data.acc_y, sensed_data.acc_z, sensed_data.acc_x_bef,
+            sensed_data.acc_y_bef, sensed_data.acc_z_bef, sensed_data.acc_x_aft, sensed_data.acc_y_aft,
+            sensed_data.acc_z_aft,
+            sensed_data.acc_xabs, sensed_data.acc_yabs, sensed_data.acc_zabs, sensed_data.acc_xabs_bef,
+            sensed_data.acc_yabs_bef, sensed_data.acc_zabs_bef,
+            sensed_data.acc_xabs_aft,
+            sensed_data.acc_yabs_aft, sensed_data.acc_zabs_aft, sensed_data.battery_level,
+            sensed_data.charging_true_count,
+            sensed_data.charging_ac, sensed_data.charging_usb,
+            sensed_data.charging_unknown, sensed_data.minutes_elapsed, sensed_data.hours_elapsed,
+            sensed_data.weekend, sensed_data.radius_of_gyration,
+            sensed_data.screen_on_count, sensed_data.screen_off_count]
     prediction = rf.rf_predict([data])
     print("Prediction:", prediction)
     return {"message": msg,
-            "request": prediction_data,
+            "sensed data": sensed_data,
             "prediction": str(prediction)
             }
 
 
-@app.post("/predict/full-pers")
-async def test(sensed_data: PredictionData, user_id: str, meal_taken: Optional[str] = None):
+@app.post("/predict/{user_id}")
+async def predict_pers(user_id: str, sensed_data: SensedData, meal_taken: Optional[str] = None):
     data = [sensed_data.acc_x, sensed_data.acc_y, sensed_data.acc_z, sensed_data.acc_x_bef,
             sensed_data.acc_y_bef, sensed_data.acc_z_bef, sensed_data.acc_x_aft, sensed_data.acc_y_aft,
             sensed_data.acc_z_aft,
