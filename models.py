7de1eae5b93e4885a2ebd09465e2e265cd4c072c
Anju Chamantha <chamantha97anju@gmail.com>
2022-03-08 21:23:09 +0530

    Database : Dataset Table

---
+++ models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Dataset(Base):
+    __tablename__ = "dataset"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+    user_id = Column(String, index=True)
+    meal_taken = Column(Float, index=True)
+
+    acc_x = Column(Float)
+    acc_y = Column(Float)
+    acc_z = Column(Float)
+    acc_x_bef = Column(Float)
+    acc_y_bef = Column(Float)
+    acc_z_bef = Column(Float)
+    acc_x_aft = Column(Float)
+    acc_y_aft = Column(Float)
+    acc_z_aft = Column(Float)
+    acc_xabs = Column(Float)
+    acc_yabs = Column(Float)
+    acc_zabs = Column(Float)
+    acc_xabs_bef = Column(Float)
+    acc_yabs_bef = Column(Float)
+    acc_zabs_bef = Column(Float)
+    acc_xabs_aft = Column(Float)
+    acc_yabs_aft = Column(Float)
+    acc_zabs_aft = Column(Float)
+
+    battery_level = Column(Float)
+    charging_true_count = Column(Float)
+    charging_ac = Column(Float)
+    charging_usb = Column(Float)
+    charging_unknown = Column(Float)
+
+    minutes_elapsed = Column(Float)
+    hours_elapsed = Column(Float)
+    weekend = Column(Float)
+    radius_of_gyration = Column(Float)
+
+    screen_on_count = Column(Float)
+    screen_off_count = Column(Float)
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
---
+++ schemas.py
@@ -0,0 +1,71 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# create
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+# read
+class Item(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+# ---------------------
+class DatasetBase(BaseModel):
+    # user_id: str
+    # meal_taken: float
+
+    acc_x: float
+    acc_y: float
+    acc_z: float
+    acc_x_bef: float
+    acc_y_bef: float
+    acc_z_bef: float
+    acc_x_aft: float
+    acc_y_aft: float
+    acc_z_aft: float
+    acc_xabs: float
+    acc_yabs: float
+    acc_zabs: float
+    acc_xabs_bef: float
+    acc_yabs_bef: float
+    acc_zabs_bef: float
+    acc_xabs_aft: float
+    acc_yabs_aft: float
+    acc_zabs_aft: float
+
+    battery_level: float
+    charging_true_count: float
+    charging_ac: float
+    charging_usb: float
+    charging_unknown: float
+
+    minutes_elapsed: float
+    hours_elapsed: float
+    weekend: float
+    radius_of_gyration: float
+
+    screen_on_count: float
+    screen_off_count: float
+
+
+class DatasetCreate(DatasetBase):
+    pass
+
+
+class Dataset(DatasetBase):
+    id: int
+
+    class Config:
+        orm_mode = True
