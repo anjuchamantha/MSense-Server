ad43d70a0038fb71363b78965cc5ec751ba03392
Anju Chamantha <chamantha97anju@gmail.com>
2022-03-08 21:24:02 +0530

    Production = True

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-production = False
+production = True
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 if not production:
